4743d82917cd437d2900762dc735c201ffeef531
Km Sumanth <sumanthkm757@gmail.com>
2026-05-07 20:43:28 +0530

    Switched Railway to Docker

--- db.php
+++ db.php
@@ -1,24 +1,16 @@
 <?php
 
-$host = $_ENV['MYSQLHOST'];
-$dbname = $_ENV['MYSQLDATABASE'];
-$user = $_ENV['MYSQLUSER'];
-$password = $_ENV['MYSQLPASSWORD'];
+$host = getenv('MYSQLHOST');
+$dbname = getenv('MYSQLDATABASE');
+$user = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
 
-try {
+$conn = new PDO(
+    "mysql:host=$host;dbname=$dbname",
+    $user,
+    $password
+);
 
-    $conn = new PDO(
-        "mysql:host=$host;dbname=$dbname",
-        $user,
-        $password
-    );
-
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch(PDOException $e) {
-
-    die("Connection failed: " . $e->getMessage());
-
-}
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
